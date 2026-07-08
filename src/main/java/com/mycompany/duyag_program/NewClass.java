36249d66be13aa6cfeb85df8766f0a50cffe6343
CL2-PC <CL2-PC@CL2-PC27>
2026-07-08 14:25:17 +0800

    ACT2C First Program Repository

--- src/main/java/com/mycompany/duyag_program/NewClass.java
+++ src/main/java/com/mycompany/duyag_program/NewClass.java
@@ -3,15 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.duyag_program;
-import java.util.Scanner;
+
 /**
  *
  * @author CL2-PC
  */
 public class NewClass {
     public static void main(String args[]){
-        Scanner scan = new Scanner(System.in);
-        
+        System.out.println("\|/        (__)");
+        System.out.println("    `\-----(00)");
+        System.out.println("     ||    (__)");
+        System.out.println("     ||w--||        \|/");
+        System.out.println("\|/");
     
     }
 }
