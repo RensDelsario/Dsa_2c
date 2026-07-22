f22ec7fe1e0483bbc1cb0c04693f957ffff0ad0a
CL2-PC <CL2-PC@CL2-PC27>
2026-07-22 14:50:45 +0800

    JFRAME OR KAHIT ANO NA RELATED

---
+++ src/main/java/com/mycompany/duyag_program/MsConnectAccess.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.duyag_program;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author CL2-PC
+ */
+public class MsConnectAccess {
+
+    public static Connection conn() {
+        try {
+            String url = "jdbc:ucanaccess://C:\\Users\\CL2-PC\\Documents/myDB.mdb";
+            Connection conn = DriverManager.getConnection(url);
+            return conn;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
+}
